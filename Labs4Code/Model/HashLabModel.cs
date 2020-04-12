e29b06c55ab697ceee79b2601ec3443c32064d39
Александр Мельников <crysis453@gmail.com>
2020-04-12 16:19:06 +0700

    Добавление класса HashLabModel

---
+++ Labs4Code/Model/HashLabModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace Labs4Code.Model
+{
+    /// <summary>
+    /// 3 лабораторная работа (хэш-функция)
+    /// </summary>
+    public class HashLabModel:PropertyChangedBase
+    {
+
+    }
+}
